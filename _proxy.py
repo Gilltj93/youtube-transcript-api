7856156fcfaa98c5ef143b4491d720e57fa1671f
Gilltj93 <gilltj93@gmail.com>
2025-06-17 08:29:15 -0400

    Update _proxy.py

--- _proxy.py
+++ _proxy.py
@@ -1,18 +1,15 @@
 # youtube_transcript_api/_proxy.py
-
+from typing import List
 import random
 
-class GenericProxyConfig:
-    def __init__(self, proxies, proxy_selection_method='random'):
-        self.proxies = proxies
-        self.proxy_selection_method = proxy_selection_method
 
-    def get_proxy(self):
-        if self.proxy_selection_method == 'random':
-            return random.choice(self.proxies)
-        elif self.proxy_selection_method == 'round_robin':
-            proxy = self.proxies.pop(0)
-            self.proxies.append(proxy)
-            return proxy
-        else:
-            raise ValueError("Invalid proxy selection method.")
+class GenericProxyConfig:
+    """
+    Proxy config that randomly chooses one of the given proxies.
+    """
+
+    def __init__(self, proxies: List[str]):
+        self.proxies = proxies
+
+    def get_proxy(self, url: str) -> str:
+        return random.choice(self.proxies)
