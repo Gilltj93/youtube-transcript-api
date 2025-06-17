7148716d18acd8e942bd7243433c2d800cb28f1c
Gilltj93 <gilltj93@gmail.com>
2025-06-17 05:38:04 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,6 @@
-from flask import Flask, request, jsonify
-from youtube_transcript_api import YouTubeTranscriptApi
+import requests
 
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return jsonify({"message": "YouTube Transcript API is running"})
-
-@app.route('/transcript', methods=['GET'])
-def get_transcript():
-    video_id = request.args.get('video_id')
-    if not video_id:
-        return jsonify({'error': 'Missing video_id'}), 400
-    try:
-        transcript = YouTubeTranscriptApi.get_transcript(video_id)
-        return jsonify(transcript)
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
+@app.route('/ip')
+def get_ip():
+    ip = requests.get("https://api.ipify.org").text
+    return jsonify({"server_ip": ip})
